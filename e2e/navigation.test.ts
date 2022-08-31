6f4f609501f341dfeb6cb37b562f9c3c9adaaa76
Tej Birring <tejb@speechmatics.com>
2022-08-31 15:25:03 +0100

    Add/refactor nav tests.

--- e2e/navigation.test.ts
+++ e2e/navigation.test.ts
@@ -1,135 +1,39 @@
 import { test, expect, Page } from '@playwright/test';
 
 const baseURL = 'http://localhost:3000';
+const btn = (text: string) : string => `button:has-text('${text}')`;
+const mItem = (text: string) : string => `.menu_elem:has-text('${text}')`;
+const secondsToWait = 2.5;
 
-test('Home to transcribe now navigations test', async ({ page }) => {
-  // Go to https://portal.speechmatics.com/home/
-  await page.goto('/home');
-  // Click text=Transcribe Now
-  await page.waitForTimeout(5000)
-  await page.locator('text=Transcribe Now').click();
-  await page.waitForTimeout(5000)
-  await expect(page).toHaveURL(baseURL+'/transcribe/');
-});
+function navTest(testPostfix: string, selector: string, URLtoAssert: string) {
+  test(`Navigation Test: Home → ${testPostfix}`, async ({ page }) => {
+    // Navigate to home page
+    await page.goto('/home');
+    await page.waitForTimeout(secondsToWait*1000)
+    // Click [aria-label="Accept cookies"]
+    await page.locator('[aria-label="Accept cookies"]').click();
+    // Click nav button
+    await page.locator(selector).click();
+    await page.waitForTimeout(secondsToWait*1000)
+    // Assert URL
+    await expect(page).toHaveURL(`${baseURL}${URLtoAssert}`);
+  })
+}
 
+// From Dashboard / Home
+navTest("RT Transcription", btn("Try Demo"), "/real-time-demo/");
+navTest("Getting Started", btn("Get Started"), "/getting-started/");
+navTest("Manage Access", btn("Create API Key"), "/manage-access/");
+navTest("Track Your Usage", btn("View Usage"), "/usage/");
+navTest("Learning Resources", btn("Learn"), "/learn/");
 
-test('Home get started button navigations test', async ({ page }) => {
-  // Go to https://portal.speechmatics.com/home/
-  await page.goto(baseURL+'/home/');
-  // Click text=Get Started
-  await page.waitForTimeout(5000)
-  await page.locator('text=Get Started').click();
-  await page.waitForTimeout(5000)
-  await expect(page).toHaveURL(baseURL+'/getting-started/');
-  await page.screenshot({path:"scn.png"})
-});
-
-
-test('create api key button press test from home page', async ({ page }) => {
-  // Go to https://portal.speechmatics.com/home/
-  await page.goto(baseURL+'/home/');
-  // Click [aria-label="Accept cookies"]
-  await page.locator('[aria-label="Accept cookies"]').click();
-  // Click text=Create API Key
-  await page.waitForTimeout(5000)
-  await page.locator('text=Create API Key').click();
-  await page.waitForTimeout(5000)
-  await expect(page).toHaveURL(baseURL+'/manage-access/');
-});
-
-test('view usage button press test from home page', async ({ page }) => {
-  // Go to https://portal.speechmatics.com/home/
-  await page.goto(baseURL+'/home/');
-  // Click [aria-label="Accept cookies"]
-  await page.locator('[aria-label="Accept cookies"]').click();
-  
-  // Click text=View Usage
-  await page.waitForTimeout(5000)
-  await page.locator('text=View Usage').click();
-  await page.waitForTimeout(5000)
-  await expect(page).toHaveURL(baseURL+'/usage/');
-});
-
-test('learn button press test from home page', async ({ page }) => {
-  // Go to https://portal.speechmatics.com/home/
-  await page.goto(baseURL+'/home/');
-  // Click [aria-label="Accept cookies"]
-  await page.locator('[aria-label="Accept cookies"]').click();
-  
-  // Click text=View Usage
-  await page.waitForTimeout(5000)
-  await page.locator('button:has-text("Learn")').click();
-  await page.waitForTimeout(5000)
-  await expect(page).toHaveURL(baseURL+'/learn/');
-});
-
-test('Home side bar to transcribe navigations test', async ({ page }) => {
-  // Go to https://portal.speechmatics.com/home/
-  await page.goto(baseURL+'/home/');
-  // Click text=Upload & Transcribe
-  await page.waitForTimeout(5000)
-  await page.locator('text=Upload & Transcribe').click();
-  await page.waitForTimeout(5000)
-  await expect(page).toHaveURL(baseURL+'/transcribe/');
-});
-
-test('Home side bar start using api button test', async ({ page }) => {
-  // Go to https://portal.speechmatics.com/home/
-  await page.goto(baseURL+'/home/');
-  // Click text=Upload & Transcribe
-  await page.waitForTimeout(5000)
-  await page.locator('text=Start Using API').first().click();
-  await page.waitForTimeout(5000)
-  await expect(page).toHaveURL(baseURL+'/getting-started/');
-});
-
-test('Home side bar manage-access button test', async ({ page }) => {
-  // Go to https://portal.speechmatics.com/home/
-  await page.goto(baseURL+'/home/');
-  // Click text=Upload & Transcribe
-  await page.waitForTimeout(5000)
-  await page.locator('text=Manage Access').first().click();
-  await page.waitForTimeout(5000)
-  await expect(page).toHaveURL(baseURL+'/manage-access/');
-});
-
-test('Home side bar view jobs button test', async ({ page }) => {
-  // Go to https://portal.speechmatics.com/home/
-  await page.goto(baseURL+'/home/');
-  // Click text=Upload & Transcribe
-  await page.waitForTimeout(5000)
-  await page.locator('text=View Jobs').first().click();
-  await page.waitForTimeout(5000)
-  await expect(page).toHaveURL(baseURL+'/view-jobs/');
-});
-
-test('Home side bar Track usage button test', async ({ page }) => {
-  // Go to https://portal.speechmatics.com/home/
-  await page.goto(baseURL+'/home/');
-  // Click text=Upload & Transcribe
-  await page.waitForTimeout(5000)
-  await page.locator('text=Track Usage').click();
-  await page.waitForTimeout(5000)
-  await expect(page).toHaveURL(baseURL+'/usage/');
-});
-
-test('Home side bar Manage billing button test', async ({ page }) => {
-  // Go to https://portal.speechmatics.com/home/
-  await page.goto(baseURL+'/home/');
-  // Click text=Upload & Transcribe
-  await page.waitForTimeout(5000)
-  await page.locator('text=Manage Billing').click();
-  await page.waitForTimeout(5000)
-  await expect(page).toHaveURL(baseURL+'/manage-billing/');
-});
-
-test('Home side bar learn button test', async ({ page }) => {
-  // Go to https://portal.speechmatics.com/home/
-  await page.goto(baseURL+'/home/');
-  // Click text=Upload & Transcribe
-  await page.waitForTimeout(5000)
-  await page.locator('text=Learn').first().click();
-  await page.waitForTimeout(5000)
-  await expect(page).toHaveURL(baseURL+'/learn/');
-});
-
+// From Side Nav
+navTest("Home (Side Nav)", mItem("Home"), "/home/");
+navTest("RT Transcription (Side Nav)", mItem("Real-time Demo"), "/real-time-demo/");
+navTest("Upload & Transcribe (Side Nav)", mItem("Upload & Transcribe"), "/transcribe/");
+navTest("Getting Started (Side Nav)", mItem("Start Using API"), "/getting-started/");
+navTest("Manage Access (Side Nav)", mItem("Manage Access"), "/manage-access/");
+navTest("View Jobs (Side Nav)", mItem("View Jobs"), "/view-jobs/");
+navTest("Track Usage (Side Nav)", mItem("Track Usage"), "/usage/");
+navTest("Manage Billing (Side Nav)", mItem("Manage Billing"), "/manage-billing/");
+navTest("Learning Resources (Side Nav)", mItem("Learn"), "/learn/");
