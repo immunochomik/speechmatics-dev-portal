0a71389e2aebea4613bdbdf9968f66af44c2e2f0
Tej Birring <tejb@speechmatics.com>
2022-08-31 15:23:02 +0100

    Allow e2e tests to run simultaneously with/using dev server. Also let Playwright decide the number of workers (default: 1 worker for each test file).

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 3,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -115,6 +115,7 @@ const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run dev',
     port: 3000,
+    reuseExistingServer: true
   },
 };
 
