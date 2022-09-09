48c5cf1042936ace21aacf5dc1346bfc85c60ac3
Tej Birring <tejb@speechmatics.com>
2022-09-09 14:42:09 +0100

    Fix RT-Demo test assertions. Also, programatically check that (some/any kind of) transcription is taking place.

--- e2e/helpers/generics.ts
+++ e2e/helpers/generics.ts
@@ -3,16 +3,16 @@ import testCfg from "../testConfigs";
 
 export default (page) => {
     return {
-        async wait() {
-            return page.waitForTimeout(testCfg.msToWait);
+        async wait(ms?: number) {
+            return page.waitForTimeout(ms ?? testCfg.msToWait);
         },
-        async clickElement(selector: string) {
+        async clickElement(selector: string, msWait?: number) {
             await page.locator(selector).click();
-            await page.waitForTimeout(testCfg.msToWait);
+            await page.waitForTimeout(msWait ?? testCfg.msToWait);
         },
-        async goTo(url: string) {
+        async goTo(url: string, msWait?: number) {
             await page.goto(url);
-            await page.waitForTimeout(testCfg.msToWait);
+            await page.waitForTimeout(msWait ?? testCfg.msToWait);
         },
         async takeScreenshot(name: string) {
             await page.screenshot({path: `./e2e/testOutput/screenshots/${name}.png`});
--- e2e/rtTranscription.test.ts
+++ e2e/rtTranscription.test.ts
@@ -6,11 +6,12 @@
  * created by the '_initContainer.sh' script during Docker container startup (before any tests are run).
  *
  ***/
-import { test, expect, Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import testCfg from './testConfigs'
 import rtProvisioner, { RTProvisioner } from './helpers/realtimeProvisioner'
 import virtualMic from "./helpers/virtualMic";
 import generics from "./helpers/generics";
+
 
 interface TranscribeOptions {
     language: "ar" | "bg" | "yue" | "ca" | "hr" | "cs" | "da" | "nl" | "en" | "fi" | "fr" | "de" | "el" | "hi" | "hu" | "it" | "ja" | "ko" | "lv" | "lt" | "ms" | "cmn" | "no" | "pl" | "pt" | "ro" | "ru" | "sk" | "sl" | "es" | "sv" | "tr" | "uk",
@@ -62,13 +63,12 @@ function rtTest(options: TranscribeOptions, sampleAudioFile: string) {
             provisionerCtxt.nTranscribersBefore = await provisionerCtxt.p.getNumTranscribers("en", "realtime");
         }
         // take a before screenshot
-        await g.takeScreenshot(`before_transcription_${testOutputPostfix}`);
+        await g.takeScreenshot(`${testOutputPostfix}_before_transcription`);
         // press the start button i.e. 'Start Real-time Transcription' button
-        await g.clickElement('[data-qa=button-get-transcription]');
+        await g.clickElement('[data-qa=button-get-transcription]', 5000);
         // play sample audio file through mic, wait until finished to move onto next stage
         await virtualMic.playSample(testName,  false, sampleAudioFile);
     }
-
 
     // During transcription. TODO: This could be truly concurrent with playFileToMic
     // This works for now only because transcribers take a while to get back into 'idle' state.
@@ -76,25 +76,33 @@ function rtTest(options: TranscribeOptions, sampleAudioFile: string) {
         // get (and check) during-transcription provisioner state
         if (provisionerCtxt) {
             provisionerCtxt.nTranscribersDuring = await provisionerCtxt.p.getNumTranscribers("en", "realtime");
-            expect (provisionerCtxt.nTranscribersDuring.idle === provisionerCtxt.nTranscribersBefore.idle-1 && provisionerCtxt.nTranscribersDuring.busy === provisionerCtxt.nTranscribersBefore.busy+1, "");
+            expect(provisionerCtxt.nTranscribersDuring.idle).toBe(provisionerCtxt.nTranscribersBefore.idle-1);
+            expect(provisionerCtxt.nTranscribersDuring.busy).toBe(provisionerCtxt.nTranscribersBefore.busy+1);
         }
     }
-
 
     // Stop transcription.
     {
         // press the stop button i.e. 'Stop Real-time Transcription' button
         await g.clickElement('[data-qa=button-get-transcription]');
         // take an after screenshot
-        await g.takeScreenshot(`after_transcription_${testOutputPostfix}`);
+        await g.takeScreenshot(`${testOutputPostfix}_after_transcription`);
         // get (and check) after-transcription provisioner state
         if (provisionerCtxt) {
             provisionerCtxt.nTranscribersAfter = await provisionerCtxt.p.getNumTranscribers("en", "realtime");
-            expect (provisionerCtxt.nTranscribersAfter.idle === provisionerCtxt.nTranscribersBefore.idle && provisionerCtxt.nTranscribersAfter.busy === provisionerCtxt.nTranscribersBefore.busy, "");
+            expect(provisionerCtxt.nTranscribersAfter.idle).toBe(provisionerCtxt.nTranscribersBefore.idle);
+            expect(provisionerCtxt.nTranscribersAfter.busy).toBe(provisionerCtxt.nTranscribersBefore.busy);
         }
+        // make sure transcription output area actually has some text (crude way)
+        const transcriptionOutputWords = (await page.locator(".showing-entities-written").allTextContents()).join(" ") // join all entries to a single string
+                                    .split(" ")                                                                        // split string to array of words
+                                    .filter(el=>el.length>0);                                                          // filter out empty entries i.e. ''
+        expect(transcriptionOutputWords.length).toBeGreaterThan(0);
     }
   })
 }
+
+
 
 
 // tests to execute:
