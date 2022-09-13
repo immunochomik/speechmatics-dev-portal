fee0967acec729f967fcb323b46c753bc28fc46f
Tej <76830999+Tej-Birring@users.noreply.github.com>
2022-09-13 14:56:33 +0100

    DEL-5550: Implement end-to-end tests for RT Demo (#179)
    Containerised Playwright-based end-to-end test infrastructure to enable transcription testing for RT-Demo.

---
+++ e2e/test-configs.ts
@@ -0,0 +1,15 @@
+interface E2ETestConfigs {
+  msToWait: number; // microseconds to wait after some action
+  transcriptionTests: {
+    checkTranscriberState: boolean; // use provisioning API to assert if transcribers are being utilized. WARNING: the cluster must NOT be in use!
+  };
+}
+
+const configs: E2ETestConfigs = {
+  msToWait: 2500,
+  transcriptionTests: {
+    checkTranscriberState: false
+  }
+};
+
+export default configs;
